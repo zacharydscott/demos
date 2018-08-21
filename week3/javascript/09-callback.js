9f21ccaa7b923683f806478c2827220343db3dcd
Blake Kruppa <btkruppa513@gmail.com>
2018-08-21 17:03:52 -0400

    added core JS examples

---
+++ week3/javascript/09-callback.js
@@ -0,0 +1,13 @@
+function caller (callback) {
+  console.log(`caller called with param:
+  ${callback}`)
+  callback(5,6);
+}
+
+function callback(one, two) {
+  console.log(`invoked with params: ${one} and ${two}`);
+}
+caller(callback);
+caller((one, two) => {
+  console.log(`one + two = ${one+two}`);
+})
