037bfbeecb341b99c159173658cb7a82522f3208
Blake Kruppa <btkruppa513@gmail.com>
2018-09-04 20:24:05 -0400

    updated enviornment variables to not start iwth a number

--- week3/movie-api/src/util/connection-util.ts
+++ week3/movie-api/src/util/connection-util.ts
@@ -2,7 +2,7 @@ import { Pool, Client } from 'pg';
 
 export const connectionPool = new Pool({
   user: process.env["MOVIE_DB_USERNAME"],
-  host: process.env["DB_URL"] || 'localhost',
+  host: process.env["MOVIE_DB_URL"] || 'localhost',
   database: 'postgres',
   password: process.env["MOVIE_DB_PASSWORD"],
   port: 5432,
