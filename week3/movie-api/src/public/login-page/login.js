2b2ca9d628fcc86e9b7b2de2cb9ffcd2efb48e3e
zacharydscott <zachary.douglas.scott@gmail.com>
2018-09-05 15:00:41 -0400

    testing autobuild

--- week3/movie-api/src/public/login-page/login.js
+++ week3/movie-api/src/public/login-page/login.js
@@ -1,31 +1,33 @@
 function login(event) {
   event.preventDefault();
-  let username = document.getElementById('inputUsername').value;
-  let password = document.getElementById('inputPassword').value;
+  let username = document.getElementById("inputUsername").value;
+  let password = document.getElementById("inputPassword").value;
 
   const credentials = { username, password };
-  fetch('http://localhost:9001/users/login', {
-    method: 'POST',
+  fetch("http://localhost:3000/users/login", {
+    method: "POST",
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
+      Accept: "application/json",
+      "Content-Type": "application/json"
     },
-    credentials: 'include',
+    credentials: "include",
     body: JSON.stringify(credentials)
   })
     .then(resp => {
       if (resp.status === 401) {
-        document.getElementById('error-message').innerText = 'Invalid Credentials';
+        document.getElementById("error-message").innerText =
+          "Invalid Credentials";
       } else if (resp.status === 200) {
         return resp.json();
       } else {
-        document.getElementById('error-message').innerText = 'Failed to Login at this time';
+        document.getElementById("error-message").innerText =
+          "Failed to Login at this time";
       }
-      throw 'Failed to login';
+      throw "Failed to login";
     })
     .then(resp => {
-      localStorage.setItem('user', JSON.stringify(resp));
-      window.location = 'http://localhost:9001/home/home.html';
+      localStorage.setItem("user", JSON.stringify(resp));
+      window.location = "http://localhost:9001/home/home.html";
     })
     .catch(err => {
       console.log(err);
